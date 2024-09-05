7df5282b778648238d7277fe6abfad1662f91768
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-09-05 17:44:28 +0900

    🎉 proj Start

---
+++ KEEP_v2/KEEP_v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KEEP_v2
+//
+//  Created by bibiga on 9/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KEEP_v2/KEEP_v2/KEEP_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  KEEP_v2App.swift
+//  KEEP_v2
+//
+//  Created by bibiga on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KEEP_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
