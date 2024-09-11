06d5587e5949aa516c29587889ddd93f8ba28d43
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-09-11 14:53:34 +0900

    :sparkles: Button(HomeView -> NoticeView)

---
+++ KEEP_v2/KEEP_v2/Student/NoticeView.swift
@@ -0,0 +1,18 @@
+//
+//  NoticeView.swift
+//  KEEP_v2
+//
+//  Created by bibiga on 9/11/24.
+//
+
+import SwiftUI
+
+struct NoticeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoticeView()
+}
--- KEEP_v2/KEEP_v2/Student/StudentHomeView.swift
+++ KEEP_v2/KEEP_v2/Student/StudentHomeView.swift
@@ -13,6 +13,48 @@ struct StudentHomeView: View {
             ZStack {
                 Color(hex:"F9FAFC")
                     .edgesIgnoringSafeArea(.all)
+                VStack {
+                    HStack {
+                        Image("keepchr")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 28)
+                        Spacer()
+                            .frame(width: 277)
+                        NavigationLink(destination: NoticeView()) {
+                            Image("mailalert")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 28)
+                        }
+                    }
+                    Spacer()
+                    ScrollView {
+                        NavigationLink(destination: NoticeView()) {
+                            Rectangle()
+                                .frame(width: 346, height: 59)
+                                .foregroundColor(.white)
+                                .cornerRadius(14)
+                                .shadow(radius: 3, x: 5, y: 3)
+                                .overlay {
+                                    HStack {
+                                        Text("긴급 공지")
+                                            .font(.system(size: 18, weight: .bold))
+                                            .foregroundColor(Color(hex: "F6556C"))
+                                        Text("확인하러 가기")
+                                            .font(.system(size: 18, weight: .medium))
+                                            .foregroundColor(.black)
+                                        Spacer()
+                                            .frame(width:120)
+                                        Image(systemName: "greaterthan")
+                                            .frame(width: 14)
+                                            .foregroundColor(.black)
+                                    }
+                                }
+                        }
+                    }
+                    .padding(.top, 26)
+                }
                 
             }
             .navigationBarBackButtonHidden(true)
