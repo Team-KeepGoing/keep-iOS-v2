39197680108a5a11784631d4c63febd8f4bc6da5
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-09-12 20:31:03 +0900

    :sparkles: 뷰 생성

---
+++ KEEP_v2/KEEP_v2/Teacher/Notice/NoticeWriteView.swift
@@ -0,0 +1,18 @@
+//
+//  NoticeWriteView.swift
+//  KEEP_v2
+//
+//  Created by bibiga on 9/12/24.
+//
+
+import SwiftUI
+
+struct NoticeWriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoticeWriteView()
+}
---
+++ KEEP_v2/KEEP_v2/Teacher/Search/StudentSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentSearchView.swift
+//  KEEP_v2
+//
+//  Created by bibiga on 9/12/24.
+//
+
+import SwiftUI
+
+struct StudentSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentSearchView()
+}
