c3d41dfa0c7cc5389af6c92fd5c0a7d6d13bd0c8
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-09-14 22:56:54 +0900

    :sparkles: QrView

--- KEEP_v2/KEEP_v2/Student/QrView.swift
+++ KEEP_v2/KEEP_v2/Student/QrView.swift
@@ -6,10 +6,74 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct QrView: View {
+    
+    @State private var qrCodeImage: Image?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading) {
+                Text("QR 대여")
+                    .font(.system(size: 25, weight: .semibold))
+                Spacer()
+                    .frame(height:30)
+                VStack(alignment: .leading) {
+                    Text("도서관에 배치 되어 있는")
+                    Text("QR코드 인식기에")
+                    Text("QR을 인식해주세요!")
+                }
+                .font(.system(size: 20, weight: .regular))
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 50)
+            
+            Spacer()
+                .frame(height:50)
+            
+            VStack {
+                if let qrCodeImage = qrCodeImage {
+                    qrCodeImage
+                        .resizable()
+                        .interpolation(.none)
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                } else {
+                    Text("QR 코드가 생성되지 않았습니다.")
+                }
+                
+                Button {
+                    if let email = UserDefaults.standard.string(forKey: "email") {
+                        generateQRCode(from: email)
+                    } else {
+                        print("이메일이 저장되어 있지 않습니다.")
+                    }
+                } label: {
+                    Text("QR코드 생성")
+                        .foregroundColor(mainColor)
+                        .font(.system(size: 15, weight: .regular))
+                }
+                .padding()
+            }
+            Spacer()
+                .frame(height: 100)
+            
+        }
+    }
+    
+    func generateQRCode(from string: String) {
+        let filter = CIFilter.qrCodeGenerator() // QR 코드 생성 필터 생성
+        let data = Data(string.utf8) // 입력된 문자열을 데이터로 변환
+        filter.setValue(data, forKey: "inputMessage") // 필터에 데이터를 설정
+        
+        guard let ciImage = filter.outputImage else { return } // 필터에서 출력된 CIImage 가져오기
+        
+        let context = CIContext() // CIContext 생성
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return } // CIImage를 CGImage로 변환
+        
+        let uiImage = UIImage(cgImage: cgImage) // CGImage로부터 UIImage 생성
+        qrCodeImage = Image(uiImage: uiImage) // UIImage를 SwiftUI의 Image로 변환하여 저장
     }
 }
 
