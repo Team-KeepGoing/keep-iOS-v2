f165589dede2ba350937865cd1e3fe1feef1dc0f
Kim ju hwan <minecraft_bos@naver.com>
2024-09-25 14:11:31 +0900

    :sparkles: NoticeViewModel

---
+++ KEEP_v2/KEEP_v2/Student/Notice/NoticeViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  NoticeViewModel.swift
+//  KEEP_v2
+//
+//  Created by 김주환 on 9/25/24.
+//
+import Foundation
+import Alamofire
+
+class NoticeViewModel: ObservableObject {
+    @Published var notices: [Notice] = []
+    
+    init() {
+        fetchNotices()
+    }
+    
+    func fetchNotices() {
+        guard let token = UserDefaultsManager.shared.loadToken() else {
+            print("토큰을 불러오지 못했습니다.")
+            return
+        }
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(token)"
+        ]
+        
+        AF.request("https://your-api-url.com/notices", headers: headers).responseDecodable(of: NoticeResponse.self) { response in
+            switch response.result {
+            case .success(let noticeResponse):
+                self.notices = noticeResponse.data
+            case .failure(let error):
+                print("Error fetching notices: \(error)")
+            }
+        }
+    }
+}
+
+struct Notice: Identifiable, Decodable {
+    let id: Int
+    let message: String
+    let teacherName: String
+    let createTime: String
+    let editTime: String
+}
+
+struct NoticeResponse: Decodable {
+    let httpStatus: String
+    let message: String
+    let data: [Notice]
+}
